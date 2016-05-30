e3f3ce9cbbd79acba1cc3100106078ff3e01bb55
Peter Szanto <git@szantocsalad.hu>
2016-05-30 17:32:34 +0200

    initial hystrix integration

--- customer-client-service/src/main/java/org/exampledriven/eureka/customer/shared/client/Application.java
+++ customer-client-service/src/main/java/org/exampledriven/eureka/customer/shared/client/Application.java
@@ -1,9 +1,18 @@
 package org.exampledriven.eureka.customer.shared.client;
 
+import org.exampledriven.eureka.customer.shared.CustomerService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableCircuitBreaker
+@EnableDiscoveryClient
+@EnableWebMvc
+@EnableFeignClients(basePackageClasses = CustomerService.class)
 public class Application {
 
     public static void main(String[] args) {
--- customer-client-service/src/main/java/org/exampledriven/eureka/customer/shared/client/rest/ClientFeignController.java
+++ customer-client-service/src/main/java/org/exampledriven/eureka/customer/shared/client/rest/ClientFeignController.java
@@ -1,5 +1,7 @@
 package org.exampledriven.eureka.customer.shared.client.rest;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.ribbon.proxy.annotation.Hystrix;
 import org.exampledriven.eureka.customer.shared.Customer;
 import org.exampledriven.eureka.customer.shared.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,10 +19,28 @@ class ClientFeignController {
     @RequestMapping(value = "/customer-client-feign/{id}", method = RequestMethod.GET, produces = "application/json")
     public Hint getCustomer(@PathVariable int id) {
 
+        return getHint(id);
+
+    }
+
+    @HystrixCommand(fallbackMethod = "fallbackGetCustomer")
+    private Hint getHint(@PathVariable int id) {
         Customer customer = customerService.getCustomer(id);
 
         return new Hint(customer, "server called using eureka with feign");
-
     }
+
+    public Hint fallbackGetCustomer(@PathVariable int id) {
+        return new Hint(null, "this result comes from a hystrix fallback");
+    }
+
+    @RequestMapping(value = "/test")
+//    @Hystrix(fallbackHandler = )
+    @HystrixCommand(fallbackMethod = "fallbackTest")
+    public String test() throws Exception {
+        throw new Exception("e");
+    }
+
+
 
 }
