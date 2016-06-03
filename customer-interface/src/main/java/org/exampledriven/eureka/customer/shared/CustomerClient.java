d8cfcf944ec5dc3f49aab03ae792daa54244eb2a
Peter Szanto <git@szantocsalad.hu>
2016-06-03 22:19:32 +0200

    cleanup

--- customer-interface/src/main/java/org/exampledriven/eureka/customer/shared/CustomerClient.java
+++ customer-interface/src/main/java/org/exampledriven/eureka/customer/shared/CustomerClient.java
@@ -2,7 +2,6 @@ package org.exampledriven.eureka.customer.shared;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
 
-//@FeignClient(value = "customer-service")
 @FeignClient(value = "customer-service", fallback = CustomerClientFallback.class)
 public interface CustomerClient extends CustomerService {
 
