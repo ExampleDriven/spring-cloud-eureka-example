ca603d3c3b3cbabcc8b099ccd55ef1fad164bddb
Peter Szanto <git@szantocsalad.hu>
2017-08-31 15:41:44 +0200

    updating Spring Boot and Spring Cloud versions

--- eureka-server/src/test/java/org/exampledriven/eureka/customer/shared/ApplicationTests.java
+++ eureka-server/src/test/java/org/exampledriven/eureka/customer/shared/ApplicationTests.java
@@ -7,17 +7,12 @@ import java.util.Map;
 import org.exampledriven.eureka.customer.server.Application;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.embedded.LocalServerPort;
-import org.springframework.boot.test.IntegrationTest;
-import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = Application.class)
